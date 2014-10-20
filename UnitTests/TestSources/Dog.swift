286bc2e39096be1b95cd5ca76925d09a536e0b6d
James Whitfield <jwhitfield@neilab.com>
2014-10-20 14:59:22 +0200

    1. Added misc notes to future swift ORM configuration.

--- UnitTests/TestSources/Dog.swift
+++ UnitTests/TestSources/Dog.swift
@@ -2,19 +2,36 @@
 //  Dog.swift
 //  iActiveRecord
 //
-//  Created by ghost on 10/18/14.
-//  Copyright (c) 2014 okolodev.org. All rights reserved.
+//  Created by JAMES WHITFIELD on 10/18/14.
+//  Copyright (c) NEiLAB, INC. All rights reserved.
 //
 
 import Foundation
 
 
+struct VRMappings {}
+
+struct VRConstraints {}
+
 class Dog : Animal {
 
  @NSManaged var breed:NSString;
 
- func testMethod() {
-    NSLog("this is a test ")
- }
+/*  // NOTE: PROPOSED STYLES to configure ORM:
+     class var mapping: VRMappings {
+        return VRMappings();
+     }
+
+     class var constraints1: VRConstraints {
+         return VRConstraints();
+     }
+
+
+    class var hasMany :[String:String] {
+         return ["foo" : "Animal"];
+    }
+
+*/
+
     
 }
